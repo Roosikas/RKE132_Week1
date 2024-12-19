959b7d44f830ed64a88f2152257047b2813647da
Roosikas <jan.roos@tktk.ee>
2024-12-19 21:55:43 +0200

    creat new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajad nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string userName = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+
+//Console.WriteLine("Hello"+ ", " + userName + "!");
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
